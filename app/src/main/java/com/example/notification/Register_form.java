6869f90365b86f5eec4cbbabee15fedaa7457c8e
iChris98 <aeshisilva10@gmail.com>
2020-03-27 10:10:25 -0700

    notifications

---
+++ app/src/main/java/com/example/notification/Register_form.java
@@ -0,0 +1,15 @@
+package com.example.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_form);
+
+    }
+}
